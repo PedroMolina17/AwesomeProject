c1c6fa19c565b368720cfb4f8c5ba6f4408a1bcd
pedromolina17 <pedro.molinanoa@gmail.com>
2024-04-20 21:34:46 -0500

    Database

--- db.js
+++ db.js
@@ -1,3 +1,12 @@
 import SQLite from 'react-native-sqlite-storage';
 
-const db = SQLite.openDatabase({name: 'mydb.db', location: 'default'});
+const db = SQLite.openDatabase(
+  {
+    name: 'mydatabase.db',
+    location: 'default',
+  },
+  () => console.log('Base de datos abierta'),
+  error => console.error('Error al abrir la base de datos: ', error),
+);
+
+export default db;
